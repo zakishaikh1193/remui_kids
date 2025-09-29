bbbe669582d744ab2babf31c82ef2a4fc925f859
Zaki <zaki.byline@gmail.com>
2025-09-29 12:04:46 +0530

    version.php changed to 08-02

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025012600;
+$plugin->version   = 2025080200;
 $plugin->requires  = 2022041200;
 $plugin->release   = '1.0.0';
 $plugin->component = 'theme_remui_kids';
